05f5497d2c84f4b10d4f2dcf32984333b939c4b4
unknown <sen.vikram5@gmail.com>
2018-07-08 11:49:48 +0530

    my initial commit

---
+++ BackEndSB2/src/main/java/com/auth/Boot2Angular4/Boot2Angular4Application.java
@@ -0,0 +1,12 @@
+package com.auth.Boot2Angular4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot2Angular4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot2Angular4Application.class, args);
+	}
+}
